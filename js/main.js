17a8adc45e9961f521ed2eeb8862e2b1fdf5b58d
Steven <siewertsteven@gmail.com>
2016-04-04 17:29:12 -0400

    changed buttons added security

--- js/main.js
+++ js/main.js
@@ -29,13 +29,17 @@ app.main = {
 	file: undefined,
 	reader: undefined,
 	codeArray: ["Begin Hack//:"],
+	security: 100,
+	counterHack: 0,
 	hp: 0,
 	lasthp: 0,
 	totalhp: 0,
 	increment: 0,
 	tickCount: 0,
 	tickCountTop: 60,
-	cost:({increment1: 10, increment2: 50, speed1: 10
+	upgrades : [],
+	cost:({increment1: 10, increment2: 50, speed1: 10, finalHack: 1000000000, 
+	security1: 50,
 		
 	}),
 
@@ -51,13 +55,12 @@ app.main = {
 		this.code = document.getElementById("sampleCode").textContent;
 		
 		//making buttons
-		var upgrades = document.querySelectorAll(".upgrade");
-		for (var i = 0; i<upgrades.length; ++i){
-			upgrades[i].addEventListener("click", function(){
+		this.upgrades = document.querySelectorAll(".upgrade");
+		for (var i = 0; i<this.upgrades.length; ++i){
+			app.main.upgrades[i].addEventListener("click", function(){
 				app.main.upgrade(this);
 			});
 		};
-		console.log(upgrades.length);
 		
 		/*
 		document.getElementById("tick1").addEventListener("click", function(){
@@ -80,14 +83,69 @@ app.main = {
 			this.hp -= this.cost.increment1;
 			this.increment++;
 			this.cost.increment1 += this.cost.increment1;
-			type.innerHTML = ">Increase hacks per tick " + this.cost.increment1;
+			type.innerHTML = ">Run Script " + this.cost.increment1;
+		}
+		if (type.id == "tick2"){
+			this.hp -= this.cost.increment2;
+			this.increment+= 5;
+			this.cost.increment2 += this.cost.increment2;
+			type.innerHTML = ">Setup Bot " + this.cost.increment2;
 		}
 		if (type.id == "speed1"){
 			this.hp -= this.cost.speed1;
 			this.tickCountTop--;
 			this.cost.speed1 += this.cost.speed1;
-			type.innerHTML = ">Increase tick speed " + this.cost.speed1;
+			type.innerHTML = ">Increase RAM " + this.cost.speed1;
 		}
+		if (type.id == "security1"){
+			this.hp -= this.cost.security1;
+			this.security+=100;
+			this.cost.security1 += this.cost.security1;
+			type.innerHTML = ">Go Incognito " + this.cost.security1;
+		}
+		if (type.id == "security2"){
+			this.hp -= this.cost.security1;
+			this.security+=200;
+			this.cost.security2 += this.cost.security2;
+			type.innerHTML = ">Setup Proxy " + this.cost.security2;
+		}
+		if (type.id == "finalHack" && this.hp >= this.cost.finalHack){
+			this.hp -= this.cost.finalHack;
+		}
+	},
+	
+	showButton : function(type){
+		if (type.id == "tick1"){
+			if (this.hp < this.cost.increment1){type.style.display = "none";}
+			else{type.style.display = "block";}
+		}
+		if (type.id == "tick2"){
+			if (this.hp < this.cost.increment2){type.style.display = "none";}
+			else{type.style.display = "block";}
+		}
+		if (type.id == "security1"){
+			if (this.hp < this.cost.security1 || this.counterHack <= 5){type.style.display = "none";}
+			else{type.style.display = "block";}
+			if (this.counterHack < 0){type.style.display = "none";}
+		}
+		if (type.id == "security2"){
+			if (this.hp < this.cost.security2 || this.counterHack <= 5){type.style.display = "none";}
+			else{type.style.display = "block";
+			
+}
+			
+		}
+		
+		if (type.id == "speed1"){
+			if (this.hp < this.cost.speed1){type.style.display = "none";}
+			else{type.style.display = "block";}
+		}
+		
+		if (type.id == "finalHack"){
+			if (this.totalhp < 100000){type.style.display = "none";}
+			else{type.style.display = "block";}
+		}
+		
 	},
 	
 	doMousedown : function(){
@@ -111,6 +169,7 @@ app.main = {
 	tick : function(){
 		this.hp += this.increment;
 		this.totalhp += this.increment;
+		if (this.totalhp > 500){this.counterHack ++;}
 	},
 	
 	drawHud : function(){
@@ -119,7 +178,10 @@ app.main = {
 		this.ctx.fillStyle = 'rgba(0,255,0,1)';
 		this.ctx.fillText(this.hp, 250, 50);
 		this.ctx.restore();
-		
+		//check if you buttons should show
+		for (var i = 0; i<this.upgrades.length; ++i){
+			this.showButton(this.upgrades[i]);
+		};
 				
 		if (this.lasthp != this.totalhp){
 			for (var i = this.lasthp; i<this.totalhp; i++){ 
@@ -135,6 +197,19 @@ app.main = {
 			}
 		}
 		this.lasthp = this.totalhp
+		
+		
+		//counterhack
+		this.ctx.save();
+		this.ctx.fillStyle = "red";
+		var barWidth = (this.counterHack/this.security) * 200
+		if (barWidth > 200){barWidth = 200}
+		this.ctx.fillRect (400,400, barWidth, 20);
+		
+		this.ctx.save();
+		this.ctx.strokeStyle = "green";
+		this.ctx.strokeRect(400,400,200,20);
+		this.ctx.restore();
 		
 		
 	},
